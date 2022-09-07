83fda29e17b5965b51b42fae4d87bea1a7a87381
Andreas Krejberg Deleuran <andr568j@edu.sde.dk>
2022-09-07 14:14:27 +0200

    New items

---
+++ RatRaceLibary/Items/Carrot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatRaceLibary.Items
+{
+    public class Carrot : IItem
+    {
+        public void Equip(Rat rat)
+        {
+            rat.item = this;
+            rat.Position += 2;
+        }
+
+        public void Unequip(Rat rat)
+        {
+            rat.item = null;
+            rat.Position -= 2;
+        }
+    }
+}
---
+++ RatRaceLibary/Items/UltimateDice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatRaceLibary.Items
+{
+    public class UltimateDice : IItem
+    {
+        public void Equip(Rat rat)
+        {
+            rat.item = this;
+            rat.SpacesMoved = RNG.Range(4, 9);
+        }
+
+        public void Unequip(Rat rat)
+        {
+            rat.item = null;
+            rat.SpacesMoved = RNG.Range(4, 9);
+        }
+    }
+}
